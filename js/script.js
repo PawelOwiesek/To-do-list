da368d39632cce620b4c5233f914f276ebd1d352
Pawel Owiesek <owiesek.p@gmail.com>
2023-02-12 14:47:15 +0100

    adding new tasks and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -2,31 +2,56 @@
   const tasks = [
     {
       content: "Zrobic sniadanie",
-      done:false,
+      done: false,
     },
 
     {
       content: "Zjesc kanapki",
-      done:true,
+      done: true,
     },
 
   ];
 
-  const render=()=>{
-    let htmlString="";
+  const addNewTask=(newTaskContent)=>{
+    tasks.push({
+      content:newTaskContent,
+    })
+    render();
+  };
 
-    for(const task of tasks){
-      htmlString +=`
-        <li>
+  const onFormSubmit= (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+    if(newTaskContent===""){
+      return;
+    };
+
+    addNewTask(newTaskContent)
+   
+  };
+  
+  const render = () => {
+    let htmlString = "";
+
+    for (const task of tasks) {
+      htmlString += `
+        <li
+           ${task.done ? "style=\"text-decoration: line-through\"" : ""}
+        >
            ${task.content}
         </li>
       `;
     }
-    document.querySelector(".js-tasks").innerHTML=htmlString;
+    document.querySelector(".js-tasks").innerHTML = htmlString;
   }
 
   const init = () => {
     render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit",onFormSubmit);
   };
 
   init();
